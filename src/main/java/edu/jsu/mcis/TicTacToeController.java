c73c98ffd0293db9f189dc8ea9ecb1ab093d12f0
vinesmatthew <mvines1@stu.jsu.edu>
2019-01-29 16:54:52 -0600

    Completed Controller and View

--- src/main/java/edu/jsu/mcis/TicTacToeController.java
+++ src/main/java/edu/jsu/mcis/TicTacToeController.java
@@ -10,7 +10,7 @@ public class TicTacToeController {
     public TicTacToeController(int width) {
         
         /* Initialize model, view, and width */
-
+        
         model = new TicTacToeModel(width);
         view = new TicTacToeView();
         
@@ -19,13 +19,19 @@ public class TicTacToeController {
     public void start() {
     
         /* MAIN LOOP (repeats until game is over) */
+        
+        while (!model.isGameover()) {
 
         /* Display the board using the View's "showBoard()", then use
            "getNextMove()" to get the next move from the player.  Enter
            the move (using the Model's "makeMark()", or display an error
            using the View's "showInputError()" if the move is invalid. */
 
-        // INSERT YOUR CODE HERE
+            view.showBoard(model.toString());
+            TicTacToeMove move = view.getNextMove(model.isXTurn());
+            if(!model.makeMark(move.getRow(), move.getCol()))
+                view.showInputError();
+        }
         
         /* After the game is over, show the final board and the winner */
 
